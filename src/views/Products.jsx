7a3a9d50d81a4c12d10a15296e3f8046be89ddf5
chairatnn <61372139+chairatnn@users.noreply.github.com>
2025-12-04 11:55:48 +0700

    first commit

---
+++ src/views/Products.jsx
@@ -0,0 +1,10 @@
+export default function Products() {
+    return (
+        <div>
+            <h1>Products</h1>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt, veniam consequatur. Atque veniam sint optio perferendis sequi incidunt harum tempora, explicabo provident itaque soluta commodi fuga porro, qui laboriosam vero!
+            </p>
+        </div>
+    );
+}
